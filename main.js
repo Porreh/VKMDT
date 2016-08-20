46d4db21b19c519b53cacc49d496ece196267e45
Dmytro <Porreh@gmail.com>
2016-08-20 18:13:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ class VKI {
         let url = x.response[i].url;
         let artist = x.response[i].artist;
         let title = x.response[i].title;
-        musicData.push({name: {'name': name, 'artist': artist, 'title': title}, 'url': url});
+        musicData.push({information: {'name': name, 'artist': artist, 'title': title}, url: {'url': url}});
       }
     });
     return musicData;
