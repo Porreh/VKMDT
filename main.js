faaa30a6679e3757b0e541bd18ac5a2f3100bd22
Dmytro <Porreh@gmail.com>
2016-08-20 13:38:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -45,4 +45,40 @@ class LinkGenerator {
   }
 }
 
-let au = VK.Auth.login();
+class VKData {
+  constructor() {
+    let fname;
+    let lname;
+    let id;
+  }
+
+  logIn(response) {
+    if(response.status == 'connected') {
+      if(typeof(response.session.user) == 'undefined') {
+        VK.Api.call('users.get', { uid: response.session.mid }, function(r) { 
+          fname = r.response[0].first_name;
+          lname = r.response[0].last_name; 
+          id = response.session.mid;
+        });
+      } else {   
+        fname = response.session.user.first_name;
+        lname = response.session.user.last_name;
+        id = response.session.mid;
+      } else {
+        VK.Auth.login(logIn);
+      }
+    }
+  }
+
+  start() {
+    VK.Auth.getLoginStatus(logIn);
+  }
+
+  getData() {
+    return `${fname} ${lname} ID:${id}` 
+  }
+}
+
+let data = new VKData();
+data.start();
+console.log(data.getData());
