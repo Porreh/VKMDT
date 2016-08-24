87d63cdf855c1fdf4fe1c67eaf1b834b5ddc3635
Dmytro <Porreh@gmail.com>
2016-08-24 11:45:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -110,8 +110,8 @@ function getAllSongs(ID) {
 }
 
 function downloadAllSongs(ID = vk.id) {
-  //downloader.getFiles(getAllSongs(ID));
-  downloader.getFiles(file);
+  downloader.getFiles(getAllSongs(ID));
+  //downloader.getFiles(file);
 }
 
 //downloader.getFiles(vk.getAllAudioData());
