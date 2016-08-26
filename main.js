534aaa5be3be91a77031aecfc1f618791fb502ae
Dmytro <Porreh@gmail.com>
2016-08-26 12:45:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -29,6 +29,26 @@ class Downloader {
   //   //console.log(`Загрузка: "${filename}"`);
   // }
   
+  saveFiles3(url, artist, title) {
+    fetch(url)  
+      .then(  
+        function(response) {  
+          if (response.status !== 200) {  
+            console.log('Looks like there was a problem. Status Code: ' +  
+            response.status);  
+            return;  
+          }
+          
+          response.json().then(function(data) {  
+            console.log(data);  
+          });  
+        }
+      )
+      .catch(function(err) {  
+        console.log('Fetch Error :-S', err);  
+      });
+  }
+  
   log(x) {
     console.info(x);
   }
