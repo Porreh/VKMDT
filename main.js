ad81b4dee957e7c37f630b1356bfde024faba3eb
Dmytro <Porreh@gmail.com>
2016-09-06 10:51:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -105,6 +105,8 @@ class VKI {
       } else {
         let audioData = [];
         for (let i = 1; i <= x.response[0]; i++) {
+          let r = x.response[i];
+          console.log(r.url);
           let url = x.response[i].url;
           let artist = x.response[i].artist;
           let title = x.response[i].title;
