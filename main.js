6bf93ddb3aa4c43e099cf72c9c81a9db74bfc4bd
Dmytro <Porreh@gmail.com>
2016-09-06 14:29:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -123,7 +123,7 @@ let vk = new VKI();
 let downloader = new Downloader();
 
 function getAllSongs(ID) {
-  vk.getAllAudioData(ID, downloader.getFiles(x));
+  vk.getAllAudioData(ID, downloader.getFiles());
 }
 
 //getAllSongs(254268339);
